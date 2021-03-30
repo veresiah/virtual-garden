592b13ffaf13267dbe7d242fa49a532e00cb0082
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-03-30 15:41:12 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'rest-client'
+    hash = RestClient.get"https://trefle.io/api/v1/plants?filter_not%5Bmaximum_height_cm%5D=null&filter%5Bligneous_type%5D=tree&order%5Bmaximum_height_cm%5D=desc&token=#{ENV['API_KEY']}"
+    plant_data = JSON.parse(hash)["data"]
+    plant_data.each do |plant|
+        Plant.create(
+        common_name: plant["common_name"],
+        scientific_name: plant["scientific_name"],
+        image_url: plant["image_url"]
+        )
+    end 
